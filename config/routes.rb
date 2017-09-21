519912a66c1f0a4e12ed3836ab421fb761bcba43
davidmuhlberger <david.muhlberger@gmail.com>
2017-09-21 23:21:18 +0200

    nouvelles jauges analyse financière, suppression de la sticky bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get "/villes/:nom/indicateurs" => "villes#show_indicateurs", as: 'ville_indicateurs'
-  get "/villes/:nom/budget" => "villes#show_budget", as: 'ville_budget'
+  get "/villes/:nom/depenses" => "villes#show_depenses", as: 'ville_depenses'
+   get "/villes/:nom/finances" => "villes#show_finances", as: 'ville_finances'
   get "/villes/:nom/fiscalite" => "villes#show_fiscalite", as: 'ville_fiscalite'
   get "/projet" => "pages#projet", as: 'pages_projet'
   get "/lexique" => "pages#lexique", as: 'pages_lexique'
