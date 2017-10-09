59cc678b6f5a354b877bee9e5fa7901e825c3b6b
davidmuhlberger <david.muhlberger@gmail.com>
2017-10-09 22:31:25 +0200

    autocomplete avec limite de caractères et début seulement

--- app/assets/javascripts/search.js.coffee
+++ app/assets/javascripts/search.js.coffee
@@ -1,3 +1,4 @@
 jQuery ->
   $("*[id=search]").autocomplete
+    minLength: 3
     source: $('#search').data('autocomplete-source')
--- app/controllers/search_suggestions_controller.rb
+++ app/controllers/search_suggestions_controller.rb
@@ -1,6 +1,6 @@
 class SearchSuggestionsController < ApplicationController
   def index
-    @search_suggestions = Ville.order(:nom_propre_et_dep).where("nom_propre_et_dep ilike ?", "%#{params[:term]}%")
+    @search_suggestions = Ville.order(:nom_propre_et_dep).where("nom_propre_et_dep ilike ?", "#{params[:term]}%")
     render json: @search_suggestions.map(&:nom_propre_et_dep)
   end
 end
